fea6f6467fb5b9cf1df5177f06f821401abfe89d
lukeskw <lucasporfirioa@gmail.com>
2024-02-10 17:56:09 -0300

    ft: adding state to search notes and debouncing the input; notes state start with local storage data

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,17 +1,56 @@
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 import mainLogo from '../assets/main-logo.svg'
 import { NewNoteCard } from '../components/new-note-card'
 import { NoteCard } from '../components/note-card'
+import { useDebounce } from 'use-debounce'
+
+interface Note {
+  id: string
+  date: Date
+  content: string
+}
 
 export function HomePage() {
-  const [notes, setNotes] = useState([
-    {
+  const [notes, setNotes] = useState<Note[]>(() => {
+    const notesOnLocalStorage = localStorage.getItem('notes')
+
+    if (notesOnLocalStorage) {
+      return JSON.parse(notesOnLocalStorage)
+    }
+    return []
+  })
+
+  const [search, setSearch] = useState('')
+
+  const [debouncedSearch] = useDebounce(search, 300)
+
+  function onNoteCreated(content: string) {
+    const newNote = {
       id: crypto.randomUUID(),
       date: new Date(),
-      content:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro id nulla illo, minus nemo mollitia?',
-    },
-  ])
+      content,
+    }
+
+    const notesArr = [newNote, ...notes]
+
+    setNotes(notesArr)
+
+    localStorage.setItem('notes', JSON.stringify(notesArr))
+  }
+
+  function handleSearch(event: ChangeEvent<HTMLInputElement>) {
+    event.preventDefault()
+
+    const query = event.target.value
+    setSearch(query)
+  }
+
+  const filteredNotes =
+    debouncedSearch !== ''
+      ? notes.filter((note) =>
+          note.content.toLowerCase().includes(debouncedSearch.toLowerCase()),
+        )
+      : notes
 
   return (
     <div className="flex w-screen flex-col space-y-6 px-8 py-8 xl:mx-auto xl:my-12 xl:max-w-7xl">
@@ -23,38 +62,20 @@ export function HomePage() {
           name="search"
           placeholder="Search on your notes..."
           className="w-full bg-transparent text-xl font-semibold tracking-tight outline-none placeholder:text-slate-500 lg:text-3xl"
+          value={search}
+          onChange={handleSearch}
         />
       </form>
 
       <div className="h-px bg-slate-700" />
 
       <div className="auto-rows-fixed grid grid-cols-1 gap-6 md:grid-cols-2 xl:grid-cols-3">
-        <NewNoteCard />
+        <NewNoteCard onNoteCreated={onNoteCreated} />
+
+        {filteredNotes?.map((note) => {
+          return <NoteCard key={note.id} note={note} />
+        })}
       </div>
     </div>
   )
-}
-
-{
-  /* <NoteCard
-          note={{
-            date: new Date('2024-02-08T12:00:00'),
-            content:
-              'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et error dolor dignissimos illum voluptas quia voluptatibus explicabo quo, est accusamus! Porro ducimus animi vel esse explicabo quisquam odio molestias natus.',
-          }}
-        />
-        <NoteCard
-          note={{
-            date: new Date('2024-02-06T12:00:00'),
-            content:
-              'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et error dolor dignissimos illum voluptas quia voluptatibus explicabo quo, est accusamus! Porro ducimus animi vel esse explicabo quisquam odio molestias natus.',
-          }}
-        />
-        <NoteCard
-          note={{
-            date: new Date('2024-02-04T12:00:00'),
-            content:
-              'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et error dolor dignissimos illum voluptas quia voluptatibus explicabo quo, est accusamus! Porro ducimus animi vel esse explicabo quisquam odio molestias natus.',
-          }}
-        /> */
 }
